228eeb2346ebf922d3bf4b89969efd0f9b1b563c
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-12-19 10:10:59 +0530

    add landing page

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React, { ReactNode } from 'react'
+
+const HomePageLayout = ({children} : {children:ReactNode}) => {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+
+export default HomePageLayout
