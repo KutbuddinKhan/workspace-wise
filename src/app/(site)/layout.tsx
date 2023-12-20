0fd2ad6ef7349499248097f8f5a4347ca858c86d
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-12-20 09:33:10 +0530

    add Header and Authentication with email

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/landing-page/header'
 import { Button } from '@/components/ui/button'
 import React, { ReactNode } from 'react'
 
 const HomePageLayout = ({children} : {children:ReactNode}) => {
   return (
     <main>
+      <Header />
       {children}
     </main>
   )
